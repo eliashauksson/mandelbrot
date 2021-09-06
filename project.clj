05326a01faf752a10e329e676a4ad2a1db0d833f
Elias Hauksson <eliashauksson@protonmail.com>
2021-09-06 17:25:40 +0200

    changed description and url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mandelbrot-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Mandelbrot set generator"
+  :url "https://github.com/eliashauksson/mandelbrot-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
